cd8a42dfd335cb48968a9d939cbf0524f5f4f4ed
zxc8027 <zxc8027@g.rit.edu>
2022-04-17 16:52:05 -0400

    Replace API Gateway URL in HTML file.

--- lambda/ReplaceApiGatewayUrl.py
+++ lambda/ReplaceApiGatewayUrl.py
@@ -1,9 +1,59 @@
+import boto3
+import json
+import os
+import urllib.request
+
+def sendCloudFormationSuccess(event, context):
+    """Sends a response to CloudFormation that the given operation completed.
+    """
+
+    try:
+        request = urllib.request.Request(event["ResponseURL"], data=json.dumps({
+            "Status": "SUCCESS",
+            "PhysicalResourceId": context.log_stream_name,
+            "StackId": event["StackId"],
+            "RequestId": event["RequestId"],
+            "LogicalResourceId": event["LogicalResourceId"],
+        }).encode(), method="PUT")
+        urllib.request.urlopen(request)
+    except Exception:
+        pass
+
+
 def lambda_handler(event, context):
     """Handles the Lambda request.
     """
 
-    # TODO: Implement
+    # Ignore the request if it is from CloudFormation and it isn't a resource create.
+    # This will happen when a resource is being deleted or updated.
+    if "RequestType" in event.keys() and event["RequestType"] != "Create":
+        sendCloudFormationSuccess(event, context)
+        return {
+            "statusCode": 200,
+            "body": "",
+        }
 
+    # Read the index.html page file.
+    s3Client = boto3.client("s3")
+    indexHtmlFile = s3Client.get_object(
+        Bucket=os.environ["BeatsmthFilesStorageBucketName"],
+        Key="index.html",
+    )["Body"].read().decode("UTF8")
+
+    # Replace the URL and replace the file.
+    indexHtmlFile = indexHtmlFile.replace("{BEATSMTH_API_GATEWAY_URL}", os.environ["ApiGatewayUrl"])
+    s3Client.put_object(
+        Body=indexHtmlFile.encode(),
+        Bucket=os.environ["BeatsmthFilesStorageBucketName"],
+        Key="index.html",
+        ContentType="text/html",
+    )
+
+    # Respond to CloudFormation that the event completed.
+    if "ResponseURL" in event.keys():
+        sendCloudFormationSuccess(event, context)
+
+    # Return an empty response.
     return {
         "statusCode": 200,
         "body": "",
